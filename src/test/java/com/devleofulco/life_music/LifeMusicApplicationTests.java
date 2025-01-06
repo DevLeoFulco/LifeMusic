92e26cc12065429e67711b357e60fd37a34edd25
Leandresson Fulco <leandresson.fulco@dgtallab.com.br>
2025-01-06 10:31:48 -0300

    first commit

---
+++ src/test/java/com/devleofulco/life_music/LifeMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devleofulco.life_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
